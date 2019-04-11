f5e01dd43ee1ae64bd489c2869cf99cac873ae3e
Zhu wenjia <Alien@MacBook-Pro-41.local>
2019-04-11 20:16:23 +0900

    update

---
+++ test.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+// import api
+// const api = require('./api')
+// import module to read file
+// const fs = require('fs')
+// import module to resolve path
+// const path = require('path')
+// import module to deal with post data
+// const bodyParser = require('body-parser')
+// impot Express
+const express = require('express')
+const app = express()
+//  const models = require('./db')
+
+app.use(express.json())
+// app.use(api)
+
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: false}))
+
+// use static
+// app.use(express.static(path.resolve(__dirname, './view')))
+// single page app
+const port = process.env.PORT || 3000
+app.get(port, function (req, res) {
+  // const html = fs.readFileSync(path.resolve(__dirname, './view/index.html'), 'utf-8')
+  // res.send(html)
+  res.send('Helloword300')
+})
+// listen to port
+
+app.listen(port, () => console.log('server started on port', port))
